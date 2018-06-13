5ee7e0b09f14356d94d0e1c497158a99760008ec
Alex L. Deweert <alex@CogitoErgoSum.local>
2018-06-13 15:43:36 -0700

    Added db query

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+const app = express()
 
-express()
-  .get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    console.log(result);
+    res.render('/', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
